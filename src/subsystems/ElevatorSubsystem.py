dd30a40c6aaa311858bc058d7669ef1e7a23e48d
Jack Scheidt <datfatratgd@gmail.com>
2025-02-01 14:56:28 -0600

    Added motor configs and current limiter. Changed controls to dpad instead of trigger

--- src/subsystems/ElevatorSubsystem.py
+++ src/subsystems/ElevatorSubsystem.py
@@ -4,6 +4,7 @@ import wpimath.controller
 import wpimath.trajectory
 import phoenix6
 from constants import ElevatorConstants
+from phoenix6 import configs, signals
 
 
 class ElevatorSubsystem(commands2.ProfiledPIDSubsystem):
@@ -23,7 +24,7 @@ class ElevatorSubsystem(commands2.ProfiledPIDSubsystem):
             ),
             0,
         )
-        self.elevcontroller = super().getController# .controler# wpimath.controller.ProfiledPIDController(5.0, 0, 0)
+        self.elevcontroller = super().getController #wpimath.controller.ProfiledPIDController(5.0, 0, 0))
         self.elevmotor_right = phoenix6.hardware.TalonFX(ElevatorConstants.kRightMotorPort, "rio")
         self.elevmotor_left = phoenix6.hardware.TalonFX( ElevatorConstants.kLeftMotorPort, "rio")
         self.topelevmotorlimitswitch = wpilib.DigitalInput(ElevatorConstants.kTopLimitSwitchChannel)
@@ -35,9 +36,29 @@ class ElevatorSubsystem(commands2.ProfiledPIDSubsystem):
             ElevatorConstants.kVVoltSecondPerMeter,
             ElevatorConstants.kAVoltSecondSquaredPerMeter,
         )
-
+        
         # Start elevator at rest in neutral position
         self.setGoal(ElevatorConstants.kElevatorOffsetMeters)
+
+    def _configs(self) -> None:
+        leftmotor_configs = configs.MotorOutputConfigs()
+        rightmotor_configs = configs.MotorOutputConfigs()
+        leftmotor_configurator = self.elevmotor_left.configurator
+        rightmotor_configurator = self.elevmotor_right.configurator
+# set invert to CW+ and apply config change
+        leftmotor_configs.inverted = signals.InvertedValue.CLOCKWISE_POSITIVE
+        rightmotor_configs.inverted = signals.InvertedValue.COUNTER_CLOCKWISE_POSITIVE
+        rightmotor_configurator.apply(rightmotor_configs)
+        leftmotor_configurator.apply(leftmotor_configs)
+        # Create current limiter
+
+        limit_configs = configs.CurrentLimitsConfigs()
+
+        limit_configs.stator_current_limit = 120
+        limit_configs.stator_current_limit_enable = True
+
+        leftmotor_configurator.apply(limit_configs)
+        rightmotor_configurator.apply(limit_configs)
 
     def _useOutput(
         self, output: float, setpoint: wpimath.trajectory.TrapezoidProfile.State
@@ -46,10 +67,15 @@ class ElevatorSubsystem(commands2.ProfiledPIDSubsystem):
         feedforward = self.feedforward.calculate(setpoint.position, setpoint.velocity)
 
         # Add the feedforward to the PID output to get the motor output
-        self.elevmotors.setVoltage(output + feedforward)
+        self.elevmotor_right.setVoltage(output + feedforward)
+        self.elevmotor_left.setVoltage(output + feedforward)
+
     def _getMeasurement(self) -> float:
-        return self.elevmotors.set_position() + ElevatorConstants.kElevatorOffsetMeters
-        
+        return self.elevmotor_right.set_position() + ElevatorConstants.kElevatorOffsetMeters
+    
+    def _getMeasurement (self) -> float:
+        return self.elevmotor_left.set_position() + ElevatorConstants.kElevatorOffsetMeters
+    
     def disablePIDSubsystems(self) -> None:
         """Disables all ProfiledPIDSubsystem and PIDSubsystem instances.
         This should be called on robot disable to prevent integral windup."""
@@ -61,22 +87,31 @@ class ElevatorSubsystem(commands2.ProfiledPIDSubsystem):
 
     def elevatorPeriodic(self) -> None:
 
-        if self.joystick2.rightTrigger():
+        if self.joystick2.povUp():
             # Here, we run PID control like normal, with a constant setpoint of 30in (0.762 meters).
-            pidOutput = self.elevcontroller.calculate(self.elevmotors.set_position(), 1.27)
-            self.elevmotors.setVoltage(pidOutput)
-        elif self.joystick2.leftTrigger():
-            pidOutput = self.elevcontroller.calculate(self.elevmotors.set_position(), 0.762)
-            self.elevmotors.setVoltage(pidOutput)
-        elif self.joystick2.leftBumper():
-            pidOutput = self.elevcontroller.calculate(self.elevmotors.set_position(), 0.3)
-            self.elevmotors.setVoltage(pidOutput)
-        elif self.joystick2.rightBumper():
-            pidOutput = self.elevcontroller.calculate(self.elevmotors.set_position(), 0)
-            self.elevmotors.setVoltage(pidOutput)
+            pidOutput = self.elevcontroller.calculate(self.elevmotor_right.set_position(), 1.27, 
+                self.elevmotor_left.set_position(), 1.27)
+            self.elevmotor_right.setVoltage(pidOutput)
+            self.elevmotor_left.setVoltage(pidOutput)
+        elif self.joystick2.povRight():
+            pidOutput = self.elevcontroller.calculate(self.elevmotor_right.set_position(), 0.762,
+                self.elevmotor_left.set_position(), 0.762)
+            self.elevmotor_right.setVoltage(pidOutput)
+            self.elevmotor_left.setVoltage(pidOutput)
+        elif self.joystick2.povLeft():
+            pidOutput = self.elevcontroller.calculate(self.elevmotor_right.set_position(), 0.3, 
+                self.elevmotor_left.set_position(), 0.3)
+            self.elevmotor_right.setVoltage(pidOutput)
+            self.elevmotor_left.setVoltage(pidOutput)
+        elif self.joystick2.povDown():
+            pidOutput = self.elevcontroller.calculate(self.elevmotor_right.set_position(), 0,
+                self.elevmotor_left.set_position(), 0)
+            self.elevmotor_right.setVoltage(pidOutput)
+            self.elevmotor_left.setVoltage(pidOutput)
         else:
             # Otherwise we disable the motor
-            self.elevmotors.set(0.0)
+            self.elevmotor_right.set(0.0)
+            self.elevmotor_left.set(0.0)
 
 
         if self.topelevmotorlimitswitch.get():
